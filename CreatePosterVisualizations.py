f7003814a2dcdc1a89a237e6df84ca886e8adb90
Chris <quinnchr@mit.edu>
2022-12-08 02:03:14 -0500

    WIP: creating visualizations of optimal tyre schedule for both drivers in matplotlib using processed data

---
+++ CreatePosterVisualizations.py
@@ -0,0 +1,39 @@
+import matplotlib.pyplot as plt
+import sys
+from datetime import datetime
+import pandas as pd
+import matplotlib.dates
+from scipy import stats
+import numpy as np
+import csv
+import os
+
+drivers = [4,16]
+
+def GetPitStopSchedule(driverNumber):
+    row = []
+    with open('DATA/DRIVER' + str(driverNumber) + '/PitStopSchedule.csv') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        row = [int(x) for x in next(csv_reader)]
+    return row
+
+def GetTyreSchedule(driverNumber):
+    row = []
+    with open('DATA/DRIVER' + str(driverNumber) + '/TyreSchedule.csv') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        row = [x for x in next(csv_reader)]
+    return row
+
+pitStopSchedule = GetPitStopSchedule(4)
+tyreSchedule = GetTyreSchedule(4)
+
+print(tyreSchedule)
+
+
+
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+jobs = ["1","2","3"]
+ax.barh(jobs, pitStopSchedule, align='center', height=.25, color='#00ff00',label='wait time')
+plt.show()
